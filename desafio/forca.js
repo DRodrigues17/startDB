b9adbf83b33b911a4d17437d2d71912da522dbb4
DRodrigues17 <adanielluiz71@gmail.com>
2022-07-20 14:04:44 -0300

    dia 2- não funcional

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,4 +1,7 @@
 class Forca {
+  letrasChutadas = [];
+  vidas = 6;
+  palavra = [];
 
   chutar(letra) {
 
@@ -15,29 +18,52 @@ class Forca {
         }
     }
 
-    function incluirLetrasChutadas(val, array){
-      if (array.some(val)){
+    function incluirLetrasChutadas(val, arr){
+      const contemLetra = arr.some(element => {
+        return element.toLowerCase() === val.toLowerCase();
+      });
+      
+
+      if(contemLetra){
           console.log("a letra em questão já foi digitada, segue abaixo todas as letras já digitadas");
           console.log(array);
       }
+      letrasChutadas.push(val);
     }
 
-    if(isNum(letra) && letra != undefined){
+    function validarLetrasPalavra(val){
+      incluirLetrasChutadas(val, letrasChutadas);
+      if (!palavraSecreta.includes(val)){
+        this.vidas = -1;
+      }
+      
+    }
+
+    if(isNum(letra)){
       console.log("digite apenas letras");
       return;
     }
     validarQuantidadeDeDigitos(letra);    
-    incluirLetrasChutadas(letra);
+    validarLetrasPalavra(letra);
+    this.buscarEstado();
    }
   
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  buscarEstado() { 
+    if (this.vidas == 0){
+      return "perdeu";
+    }
+    if (this.palavra.length == 6 && this.vidas > 0){
+      return "ganhou";
+    }
+    return "aguardando chute";
+  }
 
   buscarDadosDoJogo() {
       return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+          letrasChutadas: this.letrasChutadas, // Deve conter todas as letras chutadas
+          vidas: this.vidas, // Quantidade de vidas restantes
+          palavra: this.palavra // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
       }
   }
 }
